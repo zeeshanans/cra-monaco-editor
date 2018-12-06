93448d23cfefd7cba982f01e221d044cea297ee4
Zeeshan Ansari <zeeshanansari@Zeeshans-MacBook-Air.local>
2018-12-06 15:13:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+      width: '0',
+      height: '0'
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  editorDidMount(editor, monaco) {
+    //console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    //console.log('onChange', newValue, e);
+  }
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
   render() {
+    const code = this.state.code;
+    const options = {
+      selectOnLineNumbers: true,
+      roundedSelection: false,
+      readOnly: false,
+      cursorStyle: 'line',
+      automaticLayout: false,
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MonacoEditor
+        height={this.state.height + 'px'}
+        language="javascript"
+        value={code}
+        theme="vs-dark"
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
     );
   }
 }
